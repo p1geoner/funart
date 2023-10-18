0ede17f026645bb5cf5b918bbe0a5df88aca6eed
p1geoner <pegeoner@mail.ru>
2023-10-19 00:34:47 +0300

    fix bug

--- components/ads/ads.jsx
+++ components/ads/ads.jsx
@@ -1,8 +1,10 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 
 const Ads = () => {
   const [screenWidth, setScreenWidth] = useState();
+  const router = useRouter();
   useEffect(() => {
     if (typeof window !== "undefined") {
       setScreenWidth(window.innerWidth);
@@ -32,15 +34,47 @@ const Ads = () => {
       }
     }
   }, []);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setScreenWidth(window.innerWidth);
+      if (window.innerWidth > 480) {
+        window.yaContextCb.push(() => {
+          Ya.Context.AdvManager.render({
+            blockId: "R-A-3408799-1",
+            renderTo: "yandex_rtb_R-A-3408799-1",
+          });
+        });
+      } else {
+        if (window.innerWidth <= 480 && window.innerWidth > 360) {
+          window.yaContextCb.push(() => {
+            Ya.Context.AdvManager.render({
+              blockId: "R-A-3408799-2",
+              renderTo: "yandex_rtb_R-A-3408799-2",
+            });
+          });
+        } else {
+          window.yaContextCb.push(() => {
+            Ya.Context.AdvManager.render({
+              blockId: "R-A-3408799-3",
+              renderTo: "yandex_rtb_R-A-3408799-3",
+            });
+          });
+        }
+      }
+    }
+  }, [router]);
 
   return (
-    <>
-      {screenWidth > 480 && <div id="yandex_rtb_R-A-3408799-1"></div>}
+    <div id="advert">
+      <div id="yandex_rtb_R-A-3408799-1"></div>
+      <div id="yandex_rtb_R-A-3408799-2"></div>
+      <div id="yandex_rtb_R-A-3408799-3"></div>
+      {/* {screenWidth > 480 && }
       {screenWidth <= 480 && screenWidth > 360 && (
-        <div id="yandex_rtb_R-A-3408799-2"></div>
+        
       )}
-      {screenWidth < 360 && <div id="yandex_rtb_R-A-3408799-3"></div>}
-    </>
+      {screenWidth < 360 && } */}
+    </div>
   );
 };
 
