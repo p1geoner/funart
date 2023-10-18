12b41191bae6f3bbe6944f8ea37ab90eba5893c3
p1geoner <pegeoner@mail.ru>
2023-10-19 00:15:28 +0300

    add adaptive adverts

--- components/ads/ads.jsx
+++ components/ads/ads.jsx
@@ -1,21 +1,45 @@
 import Head from "next/head";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Ads = () => {
+  const [screenWidth, setScreenWidth] = useState();
   useEffect(() => {
     if (typeof window !== "undefined") {
-      window.yaContextCb.push(() => {
-        Ya.Context.AdvManager.render({
-          blockId: "R-A-3408799-1",
-          renderTo: "yandex_rtb_R-A-3408799-1",
+      setScreenWidth(window.innerWidth);
+      if (window.innerWidth > 480) {
+        window.yaContextCb.push(() => {
+          Ya.Context.AdvManager.render({
+            blockId: "R-A-3408799-1",
+            renderTo: "yandex_rtb_R-A-3408799-1",
+          });
         });
-      });
+      } else {
+        if (window.innerWidth <= 480 && window.innerWidth > 360) {
+          window.yaContextCb.push(() => {
+            Ya.Context.AdvManager.render({
+              blockId: "R-A-3408799-2",
+              renderTo: "yandex_rtb_R-A-3408799-2",
+            });
+          });
+        } else {
+          window.yaContextCb.push(() => {
+            Ya.Context.AdvManager.render({
+              blockId: "R-A-3408799-3",
+              renderTo: "yandex_rtb_R-A-3408799-3",
+            });
+          });
+        }
+      }
     }
   }, []);
 
   return (
     <>
-      <div id="yandex_rtb_R-A-3408799-1"></div>
+      {screenWidth > 480 && <div id="yandex_rtb_R-A-3408799-1"></div>}
+      {screenWidth <= 480 && screenWidth > 360 && (
+        <div id="yandex_rtb_R-A-3408799-1"></div>
+      )}
+      {screenWidth < 360 && <div id="yandex_rtb_R-A-3408799-1"></div>}
     </>
   );
 };
