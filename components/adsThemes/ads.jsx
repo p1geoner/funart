840fc9eeae40c4a596305b6617be1bc1a572cef7
p1geoner <pegeoner@mail.ru>
2023-11-04 21:49:03 +0300

    fix: left-side adverts

--- components/adsThemes/ads.jsx
+++ components/adsThemes/ads.jsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
-
+import styles from "./ads.module.css";
 const Ads = () => {
   const [screenWidth, setScreenWidth] = useState();
   const router = useRouter();
@@ -9,53 +9,53 @@ const Ads = () => {
       setScreenWidth(window.innerWidth);
 
       if (window.innerWidth > 1000) {
-        window.yaContextCb.push(()=>{
+        console.log(6);
+        window.yaContextCb.push(() => {
           Ya.Context.AdvManager.render({
-            "blockId": "R-A-3408799-5",
-            "renderTo": "yandex_rtb_R-A-3408799-5"
+            blockId: "R-A-3408799-6",
+            renderTo: "yandex_rtb_R-A-3408799-6",
           });
-        });    
-
-      }
-
-      if (window.innerWidth <= 1000 && window.innerWidth > 768) {
-          window.yaContextCb.push(()=>{
-            Ya.Context.AdvManager.render({
-              "blockId": "R-A-3408799-6",
-              "renderTo": "yandex_rtb_R-A-3408799-6"
-            });
-          }); 
-
-      }
-
-      if (window.innerWidth <= 768 && window.innerWidth > 480){
+        });
+      } else {
+        if (window.innerWidth <= 1000 && window.innerWidth > 768) {
+          console.log(5);
           window.yaContextCb.push(() => {
             Ya.Context.AdvManager.render({
-              blockId: "R-A-3408799-4",
-              renderTo: "yandex_rtb_R-A-3408799-4",
+              blockId: "R-A-3408799-5",
+              renderTo: "yandex_rtb_R-A-3408799-5",
             });
           });
-        
-      }
-
-      if (window.innerWidth <= 480 && window.innerWidth > 360){
-        window.yaContextCb.push(()=>{
-          Ya.Context.AdvManager.render({
-            "blockId": "R-A-3408799-3",
-            "renderTo": "yandex_rtb_R-A-3408799-3"
-          })
-        })
-        
-      }
-
-      if (window.innerWidth <= 360){
-        window.yaContextCb.push(()=>{
-          Ya.Context.AdvManager.render({
-            "blockId": "R-A-3408799-2",
-            "renderTo": "yandex_rtb_R-A-3408799-2"
-          })
-        })
-    
+        } else {
+          if (window.innerWidth <= 768 && window.innerWidth > 480) {
+            console.log(4);
+            window.yaContextCb.push(() => {
+              Ya.Context.AdvManager.render({
+                blockId: "R-A-3408799-4",
+                renderTo: "yandex_rtb_R-A-3408799-4",
+              });
+            });
+          } else {
+            if (window.innerWidth <= 480 && window.innerWidth > 360) {
+              console.log(3);
+              window.yaContextCb.push(() => {
+                Ya.Context.AdvManager.render({
+                  blockId: "R-A-3408799-3",
+                  renderTo: "yandex_rtb_R-A-3408799-3",
+                });
+              });
+            } else {
+              if (window.innerWidth <= 360) {
+                console.log(2);
+                window.yaContextCb.push(() => {
+                  Ya.Context.AdvManager.render({
+                    blockId: "R-A-3408799-2",
+                    renderTo: "yandex_rtb_R-A-3408799-2",
+                  });
+                });
+              }
+            }
+          }
+        }
       }
     }
   }, []);
@@ -63,56 +63,51 @@ const Ads = () => {
     if (typeof window !== "undefined") {
       setScreenWidth(window.innerWidth);
       if (window.innerWidth > 1000) {
-        window.yaContextCb.push(()=>{
+        window.yaContextCb.push(() => {
           Ya.Context.AdvManager.render({
-            "blockId": "R-A-3408799-6",
-            "renderTo": "yandex_rtb_R-A-3408799-6"
+            blockId: "R-A-3408799-6",
+            renderTo: "yandex_rtb_R-A-3408799-6",
           });
-        });    
-            
+        });
       }
-        if (window.innerWidth <= 1000 && window.innerWidth > 768) {
-          window.yaContextCb.push(()=>{
-            Ya.Context.AdvManager.render({
-              "blockId": "R-A-3408799-5",
-              "renderTo": "yandex_rtb_R-A-3408799-5"
-            });
-          }); 
-
-        }
-        if (window.innerWidth <= 768 && window.innerWidth > 480){
-          window.yaContextCb.push(() => {
-            Ya.Context.AdvManager.render({
-              blockId: "R-A-3408799-4",
-              renderTo: "yandex_rtb_R-A-3408799-4",
-            });
+      if (window.innerWidth <= 1000 && window.innerWidth > 768) {
+        window.yaContextCb.push(() => {
+          Ya.Context.AdvManager.render({
+            blockId: "R-A-3408799-5",
+            renderTo: "yandex_rtb_R-A-3408799-5",
           });
-        
+        });
       }
-      if (window.innerWidth <= 480 && window.innerWidth > 360){
-        window.yaContextCb.push(()=>{
+      if (window.innerWidth <= 768 && window.innerWidth > 480) {
+        window.yaContextCb.push(() => {
           Ya.Context.AdvManager.render({
-            "blockId": "R-A-3408799-3",
-            "renderTo": "yandex_rtb_R-A-3408799-3"
-          })
-        })
-        
+            blockId: "R-A-3408799-4",
+            renderTo: "yandex_rtb_R-A-3408799-4",
+          });
+        });
       }
-      if (window.innerWidth <= 360){
-        window.yaContextCb.push(()=>{
+      if (window.innerWidth <= 480 && window.innerWidth > 360) {
+        window.yaContextCb.push(() => {
           Ya.Context.AdvManager.render({
-            "blockId": "R-A-3408799-2",
-            "renderTo": "yandex_rtb_R-A-3408799-2"
-          })
-        })
-    
+            blockId: "R-A-3408799-3",
+            renderTo: "yandex_rtb_R-A-3408799-3",
+          });
+        });
+      }
+      if (window.innerWidth <= 360) {
+        window.yaContextCb.push(() => {
+          Ya.Context.AdvManager.render({
+            blockId: "R-A-3408799-2",
+            renderTo: "yandex_rtb_R-A-3408799-2",
+          });
+        });
       }
     }
   }, [router]);
 
   return (
     <div id="advert">
-      <div id="yandex_rtb_R-A-3408799-6"></div>
+      <div className={styles.maxWidth} id="yandex_rtb_R-A-3408799-6"></div>
       <div id="yandex_rtb_R-A-3408799-2"></div>
       <div id="yandex_rtb_R-A-3408799-3"></div>
       <div id="yandex_rtb_R-A-3408799-4"></div>
